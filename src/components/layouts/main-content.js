0f2d3067f8bf4a7415104166f8aff6fc5f2851ed
RamielLtd <accounts@ramiel.ltd>
2020-03-18 22:08:03 +0000

    Setting max-width for more readable experience

--- src/components/layouts/main-content.js
+++ src/components/layouts/main-content.js
@@ -6,6 +6,7 @@ import { BREAKPOINTS } from "../../constants/css-vars"
 const MainContent = styled("main")`
   @media ${BREAKPOINTS.MEDIUM} {
     flex: 1 1 70%;
+    max-width: ${rhythm(22)};
     padding-left: ${rhythm(2.5)};
   }
 `
