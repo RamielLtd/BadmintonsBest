10852a6b44f3afd72c503aeeff443f3e79815674
RamielLtd <accounts@ramiel.ltd>
2020-03-24 22:48:06 +0000

    Adding Twitter widget

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { Global, css } from "@emotion/core"
 import styled from "@emotion/styled"
+import { MDXProvider } from "@mdx-js/react"
+import { TwitterTweetEmbed as Tweet } from "react-twitter-embed"
 
 import Menu from "./menu"
 import Footer from "./footer"
+
+const shortcodes = { Tweet }
 
 const SiteContainer = styled("div")``
 const Main = styled("div")``
@@ -13,12 +17,14 @@ class Layout extends React.Component {
     const { children } = this.props
 
     return (
-      <SiteContainer>
-        <Global styles={css``} />
-        <Menu />
-        <Main>{children}</Main>
-        <Footer />
-      </SiteContainer>
+      <MDXProvider components={shortcodes}>
+        <SiteContainer>
+          <Global styles={css``} />
+          <Menu />
+          <Main>{children}</Main>
+          <Footer />
+        </SiteContainer>
+      </MDXProvider>
     )
   }
 }
--- src/components/layouts/main-content.js
+++ src/components/layouts/main-content.js
@@ -4,6 +4,11 @@ import { rhythm } from "../../utils/typography"
 import { BREAKPOINTS } from "../../constants/css-vars"
 
 const MainContent = styled("main")`
+  twitter-widget {
+    margin-top: 0 !important;
+    margin-bottom: ${rhythm(1)} !important;
+  }
+
   @media ${BREAKPOINTS.MEDIUM} {
     flex: 1 1 70%;
     max-width: ${rhythm(22)};
