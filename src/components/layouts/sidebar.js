503bf67f00cdadee36889637e7bcc4f1031034c4
RamielLtd <accounts@ramiel.ltd>
2020-03-28 20:08:55 +0000

    Fixing no spacing bug on mobile

--- src/components/layouts/sidebar.js
+++ src/components/layouts/sidebar.js
@@ -4,8 +4,11 @@ import { rhythm } from "../../utils/typography"
 import { BREAKPOINTS } from "../../constants/css-vars"
 
 const SideBar = styled("aside")`
+  margin-top: ${rhythm(2)};
+
   @media ${BREAKPOINTS.MEDIUM} {
     flex: 1 1 30%;
+    margin-top: 0;
     padding-left: ${rhythm(2.5)};
   }
 
